bcbcaadbf8ccc588d201402315b72b39e100f5fd
coolspring1293 <coolspring1293@outlook.com>
2016-05-20 10:09:06 -0700

    Remove Demoes fro testing

--- apigility/module/Status/src/Status/V1/Rest/Post/PostCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Status\V1\Rest\Post;
-
-use Zend\Paginator\Paginator;
-
-class PostCollection extends Paginator
-{
-}
--- apigility/module/Status/src/Status/V1/Rest/Post/PostEntity.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Status\V1\Rest\Post;
-
-use ArrayObject;
-
-class PostEntity extends ArrayObject
-{
-}
